0244c7fb251071f1412162f98f494e6cda45f1dc
Rahul Joshua Damanik <73378179+rahuljoshua77@users.noreply.github.com>
2021-12-14 15:53:40 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,3 @@
+token_bot="YOUR BOT TOKEN"
+username_target='-1001657737960' #for public group/channel, you can use @UserNameGroup/Channel, for private, please read the Read me first
+proxy_conf={'https': 'http:/yourproxyhere'}
